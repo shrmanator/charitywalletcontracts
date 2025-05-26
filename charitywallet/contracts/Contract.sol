0b25a78b9b88e238b7c68af00a1e867a82eebc7f
Dov <dovindustries@gmail.com>
2025-05-26 11:46:06 -0400

    Reduces stipend amount to lower payout
    Lowers the stipend to minimize operational costs or risk exposure.
    May help preserve contract funds for intended uses or adjust incentives.

--- charitywallet/contracts/Contract.sol
+++ charitywallet/contracts/Contract.sol
@@ -21,7 +21,7 @@ contract FeeSwapWithSlippagePolygon is ReentrancyGuard {
 
     uint256 public feeBasisPoints;
     uint256 public constant BASIS_POINTS = 10_000;
-    uint256 public stipendAmount = 0.05 ether;
+    uint256 public stipendAmount = 0.005 ether;
 
     address public constant WETH9 = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address public constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
